33f67f1c2202d8bc349ef6ad9a27d32891ea37b5
mikha-dev <mikha.dev@gmail.com>
2023-12-31 08:26:25 +0300

    widgets

---
+++ src/Widgets/Accordion.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Dcat\Admin\Widgets;
+
+use Dcat\Admin\Admin;
+use Illuminate\Contracts\Support\Renderable;
+
+class Accordion extends Widget implements Renderable
+{
+    protected $view = 'admin::widgets.accordion';
+
+    protected array $items = [];
+
+    public function __construct()
+    {
+        $this->id('accordion-'.uniqid());
+
+        Admin::js('@sneat-main');
+        Admin::js('@sneat-bootstrap');
+    }
+
+    /**
+     * Add item.
+     *
+     * @param string $title
+     * @param string $content
+     *
+     * @return $this
+     */
+    public function add(string $title, string $content, string $icon = NULL, bool $collapsed = false)
+    {
+        $this->items[] = [
+            'title'   => $title,
+            'content' => $content,
+            'icon' => $icon,
+            'collapsed' => $collapsed,
+        ];
+
+        return $this;
+    }
+
+    public function defaultVariables()
+    {
+        return [
+            'id'         => $this->id,
+            'items'      => $this->items,
+            'attributes' => $this->formatHtmlAttributes(),
+        ];
+    }
+}
