ded3ee9f2562c7f47d9365607d8747c47745e1b9
dtikhonov <dtihonov@code-pilots.ru>
2024-01-22 09:43:33 +0300

    D4T-11: поправил отображение steps и badge-with-icon для adminlte

--- resources/views/widgets/badge-with-icon.blade.php
+++ resources/views/widgets/badge-with-icon.blade.php
@@ -1,7 +1,7 @@
-<div class="badge bg-label-{{ $class }} {{ $fullWidth ? 'w-100 text-truncate' : '' }}">
-    <i class="{!! $icon !!} text-{{ $class }} {{ empty($title) ? '' : 'me-1' }}"></i>
+<div class="badge bg-label-{{ $bg_class }} {{ $full_width ? 'w-100 text-truncate' : '' }}">
+    <i class="{!! $icon !!} text-{{ $text_class }} {{ empty($title) ? '' : 'me-1' }}"></i>
     @if($title)
-    <span class="text-{{ $class }}">
+    <span class="text-{{ $text_class }}">
         {!! $title !!}
     </span>
     @endif
--- src/Widgets/BadgeWithIcon.php
+++ src/Widgets/BadgeWithIcon.php
@@ -10,9 +10,11 @@ class BadgeWithIcon implements Renderable
     protected string $view = 'admin::widgets.badge-with-icon';
     protected bool $fullWidth = false;
 
+    protected StyleClassType $bgClass = StyleClassType::PRIMARY;
+    protected StyleClassType $textClass = StyleClassType::PRIMARY;
+
     public function __construct(
         protected string $icon,
-        protected StyleClassType $class = StyleClassType::PRIMARY,
         protected string $title = ''
     )
     {
@@ -24,12 +26,27 @@ class BadgeWithIcon implements Renderable
         return $this;
     }
 
+    public function bgClass(StyleClassType $value): static
+    {
+        $this->bgClass = $value;
+
+        return $this;
+    }
+
+    public function textClass(StyleClassType $value): static
+    {
+        $this->textClass = $value;
+
+        return $this;
+    }
+
     public function render(): string
     {
         $data['title'] = $this->title;
         $data['icon'] = $this->icon;
-        $data['class'] = $this->class->value;
-        $data['fullWidth'] = $this->fullWidth;
+        $data['bg_class'] = $this->bgClass->value;
+        $data['text_class'] = $this->textClass->value;
+        $data['full_width'] = $this->fullWidth;
 
         return view($this->view, $data)->render();
     }
