94137a36c4aafb080452668cc901f53645b07a1d
dtikhonov <dtihonov@code-pilots.ru>
2024-01-23 08:51:30 +0300

    D4T-18: ready for sneat

--- resources/views/widgets/steps-with-progress-bar.blade.php
+++ resources/views/widgets/steps-with-progress-bar.blade.php
@@ -1,12 +1,17 @@
-<div class="d-flex flex-row align-items-stretch w-100">
+<div class="d-flex flex-lg-row flex-column align-items-stretch w-100">
     @foreach($items as $idx => $item)
-        <div class="list-group list-group-horizontal w-100 {{ $idx == 0 ? '' : 'ms-2' }}">
-            <a href="#" class="d-flex list-group-item list-group-item-action">
+        <div class="list-group list-group-horizontal w-100 {{ $idx == 0 ? '' : 'ms-lg-2 mt-2 mt-lg-0' }} bg-{{ $bg_class }}">
+
+            @if($item['href'])
+            <a href="{{ $item['href'] }}" class="d-flex list-group-item list-group-item-action {{ $item['active'] ? 'border-'.$active_class : 'border-transparent' }}">
+            @else
+            <div class="d-flex list-group-item w-100 {{ $item['active'] ? 'border-'.$active_class : 'border-transparent' }}">
+            @endif
                 <div>
                     <div class="avatar flex-shrink-0 me-3">
-                    <span class="avatar-initial rounded bg-label-primary">
-                        <i class="fa fa-calendar"></i>
-                    </span>
+                        <span class="avatar-initial rounded bg-label-{{ $item['icon_class'] }}">
+                            <i class="fa fa-calendar"></i>
+                        </span>
                     </div>
                 </div>
                 <div class="d-flex flex-column flex-grow-1 justify-content-center">
@@ -20,11 +25,23 @@
                         </div>
                         @endif
                     </div>
-                    <div class="progress" style="height: 5px;">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 95%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress {{ $item['disabled'] ? 'bg-'.$disabled_class : '' }}" style="height: 5px;">
+                        <div
+                            class="progress-bar bg-{{ $active_class }}"
+                            role="progressbar"
+                            style="width: {{ $item['percent'] }}%;"
+                            aria-valuenow="{{ $item['percent'] }}"
+                            aria-valuemin="0"
+                            aria-valuemax="100"
+                        ></div>
                     </div>
                 </div>
+            @if($item['href'])
             </a>
+            @else
+            </div>
+            @endif
+
         </div>
     @endforeach
 </div>
--- src/Widgets/StepsWithProgressBar.php
+++ src/Widgets/StepsWithProgressBar.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 namespace Dcat\Admin\Widgets;
 
+use D4T\Core\Enums\StyleClassType;
 use Illuminate\Contracts\Support\Renderable;
 
 class StepsWithProgressBar implements Renderable
@@ -9,31 +10,88 @@ class StepsWithProgressBar implements Renderable
     protected string $view = 'admin::widgets.steps-with-progress-bar';
 
     protected array $items = [];
-    protected int $activeIdx = 0;
 
-    public function add(string $title, string $description, bool $active = false, string $icon = null, string $id = null): static
+    protected StyleClassType|null $bgClass = null;
+    protected StyleClassType|null $finishedClass = null;
+    protected StyleClassType|null $activeClass = null;
+    protected StyleClassType|null $disabledClass = null;
+
+    public function add(
+        string $title,
+        string $description,
+        int $percent = 0,
+        bool $active = false,
+        bool $disabled = false,
+        string $href = '',
+        string $icon = null,
+        string $id = null
+    ): static
     {
         $id = $id ?: mt_rand();
 
         $this->items[] = [
             'title' => $title,
             'description' => $description,
+            'percent' => $percent,
+            'href' => $href,
             'icon' => $icon,
             'id' => $id,
-            'index' => count($this->items) + 1
+            'index' => count($this->items) + 1,
+            'active' => $active,
+            'disabled' => $disabled
         ];
 
-        if($active) {
-            $this->activeIdx = count($this->items) - 1;
-        }
+        return $this;
+    }
+
+    public function bgClass(StyleClassType $value): static
+    {
+        $this->bgClass = $value;
+
+        return $this;
+    }
+
+    public function finishedClass(StyleClassType $value): static
+    {
+        $this->finishedClass = $value;
+
+        return $this;
+    }
+
+    public function activeClass(StyleClassType $value): static
+    {
+        $this->activeClass = $value;
+
+        return $this;
+    }
+
+    public function disabledClass(StyleClassType $value): static
+    {
+        $this->disabledClass = $value;
 
         return $this;
     }
 
     public function render(): string
     {
-        $data['items'] = $this->items;
-        $data['active_idx'] = $this->activeIdx;
+        $data['items'] = [];
+        foreach($this->items as $item) {
+            $data['items'][$item['id']] = $item;
+
+            $data['items'][$item['id']]['icon_class'] = $this->activeClass;
+
+            if( $data['items'][$item['id']]['percent'] == 100 ) {
+                $data['items'][$item['id']]['icon_class'] = $this->finishedClass;
+            }
+            if( $data['items'][$item['id']]['disabled'] ) {
+                $data['items'][$item['id']]['icon_class'] = $this->disabledClass;
+            }
+        }
+
+        $data['bg_class'] = $this->bgClass->value;
+        $data['finished_class'] = $this->finishedClass->value;
+        $data['active_class'] = $this->activeClass->value;
+        $data['disabled_class'] = $this->disabledClass->value;
 
         return view($this->view, $data)->render();
     }
